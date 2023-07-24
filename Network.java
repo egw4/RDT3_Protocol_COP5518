d8e6b00b78757282a42278067919f2e35c89a7b6
Ben3940 <bty2015@comcast.net>
2023-07-23 21:10:12 -0400

    Began adding some functionality to the Receiver class

--- Network.java
+++ Network.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class Network {
     
 }
--- Receiver.java
+++ Receiver.java
@@ -1,3 +1,50 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class Receiver {
     
+    private DatagramSocket _socket;
+    private int            _port;
+    private boolean        _continueService;
+    
+    /**
+     * Receiver constructor
+     * 
+     * @param port: Port number that Receiver will receive and send messages on
+     */
+    public Receiver(int port) {
+        this._port = port;
+    }
+
+
+
+    /**
+     * Establishes a datagram socket to bind the specified port to
+     * 
+     * @return - 0 or a negative number describing an error code if the connection could not be established
+     */
+    public int createSocket() {
+        try {
+            this._socket = new DatagramSocket(this._port);
+        } catch (SocketException e){
+            System.err.println("Unable to create and bind to socket");
+            return -1;
+        }
+        return 0;
+    }
+
+
+    /**
+     * Closes open datagram socket
+     * 
+     * @return - 0, if no error; otherwise, a negative number indicating the error
+     */
+    public int closeSocket() {
+        this._socket.close();
+        return 0;
+    }
 }
